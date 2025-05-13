5f1922c1e8509ccf1b17cc00e8974c8e70cb3e81
maxilee0129 <27leem@smtexas.org>
2025-05-12 19:59:39 -0500

    Update LearnMode.java

--- LearnMode.java
+++ LearnMode.java
@@ -1,6 +1,24 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+// Utility class for generating unique IDs (simplified)
+class IdGenerator {
+    private static int userIdCounter = 0;
+    private static int listIdCounter = 0;
+    private static int cardIdCounter = 0;
+
+    public static int generateUserId() {
+        return ++userIdCounter;
+    }
+
+    public static int generateListId() {
+        return ++listIdCounter;
+    }
+
+    public static int generateCardId() {
+        return ++cardIdCounter;
+    }
+}
 
 class LearnMode {
     private User currentUser;
