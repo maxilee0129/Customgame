42e5874d58143647bfc349b8546e5ac755fcbcb7
maxilee0129 <27leem@smtexas.org>
2025-05-12 20:00:13 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,6 +1,23 @@
 import java.util.ArrayList;
 import java.util.HashMap; // For userPreferences
+// Utility class for generating unique IDs (simplified)
+class IdGenerator {
+    private static int userIdCounter = 0;
+    private static int listIdCounter = 0;
+    private static int cardIdCounter = 0;
 
+    public static int generateUserId() {
+        return ++userIdCounter;
+    }
+
+    public static int generateListId() {
+        return ++listIdCounter;
+    }
+
+    public static int generateCardId() {
+        return ++cardIdCounter;
+    }
+}
 class User {
     private int userId;
     private String username;
